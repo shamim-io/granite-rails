ffd568805cc421b2a0c45119399bcecd4dc9bb4b
Shamim Ahmed <theshamimahmed@gmail.com>
2021-06-25 14:21:09 +0530

    Added React.js to the project

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,4 @@ ActiveStorage.start();
 var componentRequireContext = require.context("src", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+// Support component names relative to this directory:
