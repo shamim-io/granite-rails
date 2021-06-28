5231e22d06640a20e9ca09e396f8fe959eafd3ea
Shamim Ahmed <theshamimahmed@gmail.com>
2021-06-28 23:24:04 +0530

    Added progress to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
     validates :title, presence: true, length: { maximum: 50 }
     belongs_to :user
+    enum progress: { pending: 0, completed: 1 }
     has_many :comments, dependent: :destroy
     validates :slug, uniqueness: true
     validate :slug_not_changed
